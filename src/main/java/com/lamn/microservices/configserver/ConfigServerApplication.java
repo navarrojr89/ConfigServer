43f1bbd8d442fa697f09a8712f0f11259ad9a8bd
Luis Navarro <navarrojr89@gmail.com>
2021-02-15 16:25:44 +0100

    Starting with configuration server

---
+++ src/main/java/com/lamn/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.lamn.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * The type Config server application.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
